e0873f90889e5083dec5aa6f40a1cab72b0c447f
owlicodes <owlicodes@gmail.com>
2024-10-22 13:09:01 +0800

    Adding route guard for admin pages.

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,14 +1,26 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
 import { AppAlertDialog } from "@/components/app-alert-dialog";
 import { AppSheet } from "@/components/app-sheet";
 import { AppSidebar } from "@/components/app-sidebar";
 import { QueryProvider } from "@/components/providers/query-provider";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { auth } from "@/lib/auth";
 
-export default function AdminLayout({
+export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth.api.getSession({
+    headers: headers(),
+  });
+
+  if (session?.user.role !== "admin") {
+    redirect("/");
+  }
+
   return (
     <QueryProvider>
       <SidebarProvider>
